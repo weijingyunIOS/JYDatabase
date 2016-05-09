ac6cebb681830f6f076184dea6bd7daa4f1584ea
魏景云 <wei_jingyun@outlook.com>
2016-05-09 11:46:51 +0800

    创建项目
    创建项目

---
+++ JYDatabase - OC/JYDatabase - OC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JYDatabase - OC
+//
+//  Created by weijingyun on 16/5/9.
+//  Copyright © 2016年 weijingyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
