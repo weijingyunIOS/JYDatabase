fac63c6fe99cf3fb0af93a355f846bfef79bcb1a
魏景云 <wei_jingyun@outlook.com>
2016-10-14 16:20:11 +0800

    注释

--- JYDatabase - OC/JYDatabase - OC/JYDatabase/JYDataBaseConfig.h
+++ JYDatabase - OC/JYDatabase - OC/JYDatabase/JYDataBaseConfig.h
@@ -10,6 +10,13 @@
 #define JYDataBaseConfig_h
 #import <UIKit/UIKit.h>
 
+/*
+ 虽然索引的目的在于提高数据库的性能，但这里有几个情况需要避免使用索引。使用索引时，应重新考虑下列准则：
+ 索引不应该使用在较小的表上。
+ 索引不应该使用在有频繁的大批量的更新或插入操作的表上。
+ 索引不应该使用在含有大量的 NULL 值的列上。
+ 索引不应该使用在频繁操作的列上。
+ */
 typedef NS_ENUM(NSInteger, EJYDataBaseIndex) { //索引类型
     EJYDataBaseIndexNonclustered,  // 非唯一索引
     EJYDataBaseIndexOnlyIndex,     // 唯一索引
