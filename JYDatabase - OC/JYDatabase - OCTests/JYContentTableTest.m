df80684f6465c8c14494f437cb62aea14a5a57c9
魏景云 <wei_jingyun@outlook.com>
2016-11-21 16:42:17 +0800

    测试代码修改

--- JYDatabase - OC/JYDatabase - OCTests/JYContentTableTest.m
+++ JYDatabase - OC/JYDatabase - OCTests/JYContentTableTest.m
@@ -36,7 +36,7 @@
     [[JYDBService shared] insertGradeInfo:gradeInfo];
     // 查询数据
     JYGradeInfo *queryGradeInfo = [[JYDBService shared] getGradeInfo:gradeInfo.gradeID];
-    BOOL isEqual = [self contrastModel:gradeInfo toModel:queryGradeInfo];
+    BOOL isEqual = [self gradeInfo:gradeInfo equalTo:queryGradeInfo];
     XCTAssert(isEqual);
     //修改数据再插入
     gradeInfo.gradeName = @"修改后的 gradeName";
@@ -95,15 +95,6 @@
     return classInfo;
 }
 
-- (BOOL)classInfo:(JYClassInfo *)classInfo1 equalTo:(JYClassInfo *)classInfo2{
-    
-    __block BOOL isEqual = YES;
-    NSArray<NSString *>*keys = @[@"classID",@"gradeID",@"className"];
-    isEqual &= [self mode:classInfo1 equalTo:classInfo2 forKeys:keys];
-    
-    return isEqual;
-}
-
 - (JYPersonInfo *)getPersonInfoForTeacherClassID:(NSString *)teacherClassID studentClassID:(NSString *)studentClassID{
     
     JYPersonInfo *personInfo = [[JYPersonInfo alloc] init];
@@ -130,20 +121,50 @@
     return personInfo;
 }
 
+- (BOOL)gradeInfo:(JYGradeInfo *)gradeInfo1 equalTo:(JYGradeInfo *)gradeInfo2{
+    
+    __block BOOL isEqual = YES;
+    NSArray<NSString *>*keys = @[@"gradeID",@"gradeName"];
+    isEqual &= [self mode:gradeInfo1 equalTo:gradeInfo2 forKeys:keys];
+    [gradeInfo1.allClass enumerateObjectsUsingBlock:^(JYClassInfo * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        isEqual &= [self classInfo:obj equalTo:gradeInfo2.allClass[idx]];
+    }];
+    return isEqual;
+
+}
+
+
+- (BOOL)classInfo:(JYClassInfo *)classInfo1 equalTo:(JYClassInfo *)classInfo2{
+    
+    __block BOOL isEqual = YES;
+    NSArray<NSString *>*keys = @[@"classID",@"gradeID",@"className"];
+    isEqual &= [self mode:classInfo1 equalTo:classInfo2 forKeys:keys];
+    isEqual &= [self personInfo:classInfo1.teacher equalTo:classInfo2.teacher];
+    [classInfo1.students enumerateObjectsUsingBlock:^(JYPersonInfo * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        isEqual &= [self personInfo:obj equalTo:classInfo2.students[idx]];
+    }];
+    return isEqual;
+}
+
 - (BOOL)personInfo:(JYPersonInfo *)personInfo1 equalTo:(JYPersonInfo *)personInfo2{
     
     __block BOOL isEqual = YES;
     NSArray<NSString *>*keys = @[@"PersonID",@"teacherClassID",@"studentClassID"];
     isEqual &= [self mode:personInfo1 equalTo:personInfo2 forKeys:keys];
+    [personInfo1.array.firstObject isEqualToString:personInfo2.array.firstObject];
+    [personInfo1.arrayM.firstObject isEqualToString:personInfo2.arrayM.firstObject];
+    [personInfo1.dic[@"dic11"] isEqualToString:personInfo2.dic[@"dic11"]];
+    [personInfo1.dicM[@"dic12"] isEqualToString:personInfo2.dicM[@"dic12"]];
+    // 浮点数会有些许偏差
+    isEqual &= personInfo1.integer1 == personInfo2.integer1;
+    isEqual &= personInfo1.uInteger1 == personInfo2.uInteger1;
+    isEqual &= labs(personInfo1.int1 - personInfo2.int1) < 0.00001;
+    isEqual &= personInfo1.bool1 == personInfo2.bool1;
+    isEqual &= fabs(personInfo1.double1 - personInfo2.double1) < 0.00001;
+    isEqual &= fabsf(personInfo1.float1 - personInfo2.float1) < 0.00001;
+    isEqual &= fabs(personInfo1.cgfloat1 - personInfo2.cgfloat1) < 0.00001;
     return isEqual;
 }
-
-- (BOOL)contrastModel:(JYGradeInfo *)model1 toModel:(JYGradeInfo *)model2{
-    JYPersonInfo *info1 = model1.allClass.firstObject.teacher;
-    JYPersonInfo *info2 = model2.allClass.firstObject.teacher;
-    return [self personInfo:info1 equalTo:info2];
-}
-
 
 // 只支持简单的 NSString 检测
 - (BOOL)mode:(id)model1 equalTo:(id)model2 forKeys:(NSArray<NSString *> *)keys{
